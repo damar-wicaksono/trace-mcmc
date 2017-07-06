765f3d0e943575557ee634c6065bcb4c7544aeb5
WD41 <damar.wicaksono@psi.ch>
2017-07-06 19:28:59 +0200

    Revise function documentation

--- r-scripts/GetTimeExpCO.R
+++ r-scripts/GetTimeExpCO.R
@@ -1,10 +1,8 @@
 #' Get restricted time index for Carryover output (CO <= arg [kg])
 #'
-#' \code{GetTimeIdxCO} takes the last time index in a table of time-data pairs
-#' of liquid carryover measurement in FEBA Test Series I.
 #' In the series, all liquid carryover measurement saturated the tank before 
 #' the end of the transient at \code{max_co} [kg], the capacity of the tank.
-#' As such the calibration will be conducted with respect to part of transiet
+#' As such the calibration will be conducted with respect to part of transient
 #' 
 #' @param exp_data Matrix. Time-Experimental Data pairs, 1st column is the time
 #' @param max_co Numeric. Maximum liquid carryover in [kg] measured in the tank
