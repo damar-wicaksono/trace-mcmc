77fa1c2c30c39aa5623c7cecd85b1a628532f769
WD41 <damar.wicaksono@psi.ch>
2017-07-06 17:48:40 +0200

    Add R function to get the time index for TRACE output

---
+++ r-scripts/GetTimeIdxTrc.R
@@ -0,0 +1,20 @@
+#' Get the time index for TRACE based on the experimental data time points
+#' 
+#' Note that the division between any of the time points and d_t has to yield
+#' positive integer value (strictly speaking, whole number) 
+#'
+#' @param exp_time Numeric vector. Time points of the experimental data
+#' @param d_t Numeric. Time step size of the TRACE simulation.
+#'   It is assumed that the step size is uniform, that the time grid has been 
+#'   homogenized.
+#' @return Vector of time indices in the TRACE output
+GetTimeIdxTrc <- function(exp_time, d_t = 1e-1)
+{
+    if (sum(exp_time %% d_t) != 0)
+    {
+        stop("One or more time index is not a whole number")   
+    } else 
+    {
+        return(exp_time / d_t + 1)
+    }
+}
