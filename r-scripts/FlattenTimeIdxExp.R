a88c667b6e59627e2ae0380b03a5c92391b646aa
WD41 <damar.wicaksono@psi.ch>
2017-07-09 19:53:50 +0200

    Fix implementation problem for the function on index without exclusion

--- r-scripts/FlattenTimeIdxExp.R
+++ r-scripts/FlattenTimeIdxExp.R
@@ -10,18 +10,23 @@
 #' standard deviation. At time 0 [s] the standard deviation is zero thus the 
 #' data was excluded altogether.
 #'
+#' The function can also be used for other time indices at different location 
+#' given as list
+#'
 #' @param time_idx_list List of numeric vector. The list of indices indicating
 #'  restricted time points per axial location
 #' @param n_time Numeric. The length of time points per axial location
 #' @param excl_idx Numeric. Index to exclude, but must be contiguous
 #' @return
-FlattenTimeIdxExpTC <- function(time_idx_list, n_time, excl_idx)
+FlattenTimeIdxExp <- function(time_idx_list, n_time, excl_idx=NULL)
 {
     time_idx_vec <- c()
+
     for (i in 1:length(time_idx_list))
     {
+        incl_idx <- setdiff(1:length(time_idx_list[[i]]), excl_idx)
         time_idx_vec <- c(time_idx_vec, 
-                          (time_idx_list[[i]][-excl_idx] - length(excl_idx)) + 
+                          (time_idx_list[[i]][incl_idx] - length(excl_idx)) + 
                           (i - 1) * (n_time - length(excl_idx)))
     }
 
