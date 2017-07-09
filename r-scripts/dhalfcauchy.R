f75cbb7c2b0f68f322ae032da1f05ae8a6375dde
WD41 <damar.wicaksono@psi.ch>
2017-07-09 14:28:19 +0200

    Add function to compute half Cauchy distribution density

---
+++ r-scripts/dhalfcauchy.R
@@ -0,0 +1,23 @@
+#' Half Cauchy Density (x < 0 is forced to be 0)
+#'
+#' The density of half Cauchy is twice the density of standard Cauchy because
+#' the area under the curve has to be equal to 1.0
+#' The default scale parameter value is re-defined to be 25.0
+#' 
+#' @param x Numeric vector. The vector of quantile
+#' @param location Numeric. The location parameter
+#' @param scale  Numeric. The scale parameter
+#' @param log Boolean. Log scale computation
+#' @return the density of the half Cauchy distribution
+dhalfcauchy <- function(x, location = 0, scale = 25, log = FALSE)
+{
+    if (all(x < 0))
+    {
+        return(0)
+    } else
+    {   
+        x <- x[x >= 0]  # Exclude negative value
+        
+        return(2 * dcauchy(x, location = location, scale = scale, log = log))
+    }
+}
