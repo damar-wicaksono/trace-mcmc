9ef5d39931915cfb356a2d8043780184cf139667
WD41 <damar.wicaksono@psi.ch>
2017-07-10 11:11:12 +0200

    Compiled list in the input can either be PC GP or Bias GP training runs

--- r-scripts/GetTimeIdxDP.R
+++ r-scripts/GetTimeIdxDP.R
@@ -4,13 +4,13 @@
 #' dataset can be compared with the prediction.
 #' There were 4 pressure drop measurements and all had the same time points.
 #'
-#' @param trc_data_bias List. List of compiled results from bias model 
-#'  training runs
+#' @param trc_data List. List of compiled results from TRACE model training 
+#'  either PC GP or Bias GP runs
 #' @param d_t Numeric. The time step size in the uniform time grid of full 
 #'  output space (default = 0.1 [s])
 #' @return Numeric vector. The indices from the full model output for all 
 #'  pressure drop measurements.
-GetTimeIdxDP <- function(trc_data_bias, d_t = 0.1)
+GetTimeIdxDP <- function(trc_data, d_t = 0.1)
 {
     exp_data <- trc_data_bias$exp_data[[2]]
     res_time <- GetTimeIdxTrc(exp_data[, 1], d_t = d_t)
